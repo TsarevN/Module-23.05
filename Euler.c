4f8e7003528714d71cfe00fce9d364ed8394485b
TsarevN <91004467+TsarevN@users.noreply.github.com>
2022-05-22 20:07:25 +0300

    Update and rename main.c to Euler.c

--- Euler.c
+++ Euler.c
@@ -6,7 +6,6 @@
 #include <ctype.h>
 #include <stdlib.h> 
 
-//Number 1
 int isPalindrome(long long n)
 {
 	long long reverse = 0;
@@ -468,37 +467,8 @@ void NumberFifteen()
 	printf("Answer NumberFifteen = %lld\n", matrix[0][0]);
 }
 
-//Number 2
-int ParseBrackets(char* str)
-{
-	int k = -1;
-	int count = 0;
-	int lastClosed = 0;
-	char open = '(';
-	char closed = ')';
-	for (int i = 0; i < strlen(str); i++)
-	{
-		if (str[i]==open)
-		{
-			++count;
-		}
-		else if (str[i] == closed)
-		{
-			--count;
-			lastClosed = i + 1;
-		}
-	}
-	if (count < 0)
-		return lastClosed - 1;
-	else if (count > 0)
-		return -1;
-	else
-		return 0;
-}
-
 int main()
 {
-	//Number 1
 	NumberOne();
 	NumberTwo();
 	NumberThree();
@@ -514,15 +484,4 @@ int main()
 	NumberThirteen();
 	NumberFourteen();
 	NumberFifteen();
-
-	//Number 2
-	char* str = (char*)malloc(1024 * sizeof(char));
-	if (!str)
-	{
-		printf("Memory Error");
-		return 0;
-	}
-	gets_s(str, 1024);
-	printf("%d", ParseBrackets(str));
-	
 }
